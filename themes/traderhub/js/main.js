8a3b5ce13ad8a1946b6e89acd29981e0ec3083cc
ArialBlack <andreyturik@gmail.com>
2017-02-15 20:27:43 +0200

    js changes

--- themes/traderhub/js/main.js
+++ themes/traderhub/js/main.js
@@ -6,13 +6,13 @@
 
     var config1 = {
       fields: {
-        "Name": "#crm_name",
-        "UsrSurname": "#crm_lastname",
-        "Email": "#crm_Email",
-        "MobilePhone": "#crm_phone",
-        "City": "#crm_city",
-        "Country": "#crm_country",
-        "Commentary": "#crm_body"
+        "Name": "#hiddenCRMvalues #crm_name",
+        "UsrSurname": "#hiddenCRMvalues #crm_lastname",
+        "Email": "#hiddenCRMvalues #crm_Email",
+        "MobilePhone": "#hiddenCRMvalues #crm_phone",
+        "City": "#hiddenCRMvalues #crm_city",
+        "Country": "#hiddenCRMvalues #crm_country",
+        "Commentary": "#hiddenCRMvalues #crm_body"
       },
       landingId: "8a768405-a656-43b5-8487-fdbe78fc3e0e",
       serviceUrl: "https://fondexx.bpmonline.com/0/ServiceModel/GeneratedWebFormService.svc/SaveWebFormLeadData",
@@ -21,13 +21,13 @@
 
     var confi2 = {
       fields: {
-        "Name": "#crm_name",
-        "UsrSurname": "#crm_lastname",
-        "Email": "#crm_Email",
-        "MobilePhone": "#crm_phone",
-        "City": "#crm_city",
-        "Country": "#crm_country",
-        "Commentary": "#crm_body"
+        "Name": "#hiddenCRMvalues #crm_name",
+        "UsrSurname": "#hiddenCRMvalues #crm_lastname",
+        "Email": "#hiddenCRMvalues #crm_Email",
+        "MobilePhone": "#hiddenCRMvalues #crm_phone",
+        "City": "#hiddenCRMvalues #crm_city",
+        "Country": "#hiddenCRMvalues #crm_country",
+        "Commentary": "#hiddenCRMvalues #crm_body"
       },
       landingId: "46234ad8-7d7f-4dca-a3be-6385833cc633",
       serviceUrl: "https://fondexx.bpmonline.com/0/ServiceModel/GeneratedWebFormService.svc/SaveWebFormLeadData",
@@ -36,70 +36,68 @@
 
     var config3 = {
       fields: {
-        "Name": "#crm_name",
-        "UsrSurname": "#crm_lastname",
-        "Email": "#crm_Email",
-        "MobilePhone": "#crm_phone",
-        "City": "#crm_city",
-        "Country": "#crm_country",
-        "Commentary": "#crm_body"
+        "Name": "#hiddenCRMvalues #crm_name",
+        "UsrSurname": "#hiddenCRMvalues #crm_lastname",
+        "Email": "#hiddenCRMvalues #crm_Email",
+        "MobilePhone": "#hiddenCRMvalues #crm_phone",
+        "City": "#hiddenCRMvalues #crm_city",
+        "Country": "#hiddenCRMvalues #crm_country",
+        "Commentary": "#hiddenCRMvalues #crm_body"
       },
       landingId: "21f00e1c-f758-46af-abd6-ac609a40614d",
       serviceUrl: "https://fondexx.bpmonline.com/0/ServiceModel/GeneratedWebFormService.svc/SaveWebFormLeadData",
       redirectUrl: "http://trader-hub.com/thankyou5"
     };
 
-    var submitted = 0,
-        count = 0;
-
-    Object.size = function(obj) {
-      var size = 0, key;
-      for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-      }
-      return size;
-    };
-
     function URLToArray(url) {
       var request = {},
-          pairs = url.substring(url.indexOf('?') + 1).split('&');
+          pairs = url.substring(url.indexOf('?') + 1).split('&'),
+          subValues = 0;
 
       for (var i = 0; i < pairs.length; i++) {
         var pair = pairs[i].split('=');
         var key = pair[0].substring(pair[0].lastIndexOf("[")+1,pair[0].lastIndexOf("]"));
         request[key] = decodeURIComponent(pair[1]);
-
-        if (request[key]) {
-          $('<input>').attr({
-            type: 'hidden',
-            id: 'crm_' + key,
-            name: 'crm_' + key,
-            value: request[key]
-          }).appendTo('.region-page-bottom');
-
-          count = count + 1;
-        }
       }
-
       return request;
     }
 
-    function checkUrl2CMR(){
-      var url = window.location.href;
+    function buildCRMForm(url) {
+      var submitted = URLToArray(url),
+          form = '<form id="hiddenCRMvalues">',
+          subvalues = 0;
+
+      for (key in submitted) {
+        if (submitted.hasOwnProperty(key)) {
+          //console.log(submitted[key]);
+          form = form + '<input type="hidden" id="crm_' + key + '" name="crm_' + key + '" value="' +submitted[key] + '">';
+          subvalues = subvalues + 1;
+        }
+      }
+
+      form = form + '</form>';
+      $(form).appendTo('.region-page-bottom');
+      return subvalues;
+    }
+
+    function send2CRM(){
+      var url = window.location.href,
+          count = 0;
 
       if (url.indexOf('thankyou') !== -1) {
-        submitted = URLToArray(url);
+        count = buildCRMForm(url);
 
         if (url.indexOf('thankyou1') !== -1 && count >=6) {
-          //landing.initLanding(config1);
+          console.log(count);
+          landing.initLanding(config1);
         }
 
         if (url.indexOf('thankyou2') !== -1 && count >=6) {
-          //landing.initLanding(config2);
+          landing.initLanding(config2);
         }
 
         if (url.indexOf('thankyou5') !== -1 && count >=6) {
-          //landing.initLanding(config3);
+          landing.initLanding(config3);
         }
       }
     }
@@ -150,7 +148,7 @@
       //$(document).scrollTop(300); // $(hash).offset().top
 
       webformsJob();
-      checkUrl2CMR();
+      send2CRM();
     });
 
   });
