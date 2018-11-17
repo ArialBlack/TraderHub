ca0ce3745262aa0d7cfcdd9def979f619659fe03
Andrii Turyk <aturyk@adyax.com>
2018-11-17 13:18:25 +0200

    add front coookie reading

--- themes/traderhub/js/fondexx-track-cookies.js
+++ themes/traderhub/js/fondexx-track-cookies.js
@@ -218,3 +218,31 @@
         }
     }
 })();
+
+(function ($) {
+  $(function() {
+
+    function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+
+      var $wt = $('input[name="submitted[wt]"]'),
+        wtVal = $wt.val();
+
+      if (wtVal.length < 15) {
+          var fndxxHref = readCookie('bpmHref').replace(/\//g, "\\/"),
+            fndxxRef = readCookie('fndxxRef').replace(/\//g, "\\/"),
+            fndxxTrackingId = readCookie('fndxxTrackingId'),
+            json = '["' + fndxxTrackingId +'", "' + fndxxHref +'", "' + fndxxRef + '"]';
+
+          $wt.val(json)
+      }
+  });
+}(jQuery));
